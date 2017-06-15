af2e604765508d4e3626800f61c3156298ade63f
Sean <sean.graux@laposte.net>
2017-06-15 11:43:13 +0200

    classe bouton case

---
+++ POO/src/uidelegate/BoutonCase.java
@@ -0,0 +1,14 @@
+package uidelegate;
+
+import javax.swing.JButton;
+
+public class BoutonCase extends JButton{
+	
+	private Case casePlateau;
+	
+	public BoutonCase(Case parCasePlateau){
+		super();
+		casePlateau = parCasePlateau;
+	}
+
+}
