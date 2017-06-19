136dcf7ca2e29c2f5df0104dbc65fac6600b683a
Sean <Sean@Crabe-Machine>
2017-06-19 14:24:14 +0200

    update controle

--- POO/src/controler/Controle.java
+++ POO/src/controler/Controle.java
@@ -7,8 +7,7 @@ import vue2.LancePartie;
 public class Controle {
 	private static Dimension size = new Dimension(800,700);
 	
-	public static void main(String[] args){
-		
+	public static void main(){
 		
 		LancePartie part = new LancePartie(size);
 		part.affichage();
